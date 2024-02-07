071f45c2db27e6def3c07beb80665f0b2a0217b3
Abhinit <giriabhinit02@gmail.com>
2024-02-07 15:20:23 -0500

    First assignment

---
+++ 01-image-processing/src/imageProcessing.test.ts
@@ -0,0 +1,69 @@
+import assert from "assert";
+import { COLORS, Image } from "../include/image.js";
+import { flipColors, saturateGreen } from "./imageProcessing.js";
+
+describe("saturateGreen", () => {
+  it("should maximize green in the upper left corner", () => {
+    const blackImage = Image.create(10, 15, COLORS.BLACK);
+    const gbImage = saturateGreen(blackImage);
+    const p = gbImage.getPixel(0, 0);
+
+    assert(p[0] === 0, "The red channel should be 0.");
+    assert(p[1] === 255, "The green channel should be 255.");
+    assert(p[2] === 0, "The blue channel should be 0.");
+
+    // or alternatively, using jest, if you'd like
+    // https://jestjs.io/docs/expect#toequalvalue
+    // Use expect with .toEqual to compare recursively all properties of object instances (also known as "deep" equality).
+
+    expect(p).toEqual([0, 255, 0]);
+
+    // This will produce output showing the exact differences between the two objects, which is really helpful
+    // for debugging. However, again, please use the simpler assert syntax if this is too confusing.
+    // Focus on making your tests well written and correct, rather than using one syntax or another.
+  });
+
+  it("should maximize green in the center", () => {
+    const blackImage = Image.create(10, 15, COLORS.BLACK);
+    const gbImage = saturateGreen(blackImage);
+    const p = gbImage.getPixel(5, 7);
+
+    assert(p[0] === 0, "The red channel should be 0.");
+    assert(p[1] === 255, "The green channel should be 255.");
+    assert(p[2] === 0, "The blue channel should be 0.");
+  });
+
+  // More tests for removeRed go here.
+});
+
+describe("flipColors", () => {
+  it("should correctly flip top left corner", () => {
+    const whiteImage = Image.create(10, 10, COLORS.WHITE);
+    // A white image is not particularly helpful in this context
+    whiteImage.setPixel(0, 0, [100, 0, 150]);
+    const flippedWhiteImage = flipColors(whiteImage);
+    const p = flippedWhiteImage.getPixel(0, 0);
+
+    assert(p[0] === 75);
+    assert(p[1] === 125);
+    assert(p[2] === 50);
+  });
+
+  // More tests for flipColors go here.
+});
+
+describe("mapLine", () => {
+  // Tests for mapLine go here.
+});
+
+describe("imageMap", () => {
+  // Tests for imageMap go here.
+});
+
+describe("mapToGreen", () => {
+  // Tests for mapToGreen go here.
+});
+
+describe("mapFlipColors", () => {
+  // Tests for mapFlipColors go here.
+});
